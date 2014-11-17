35330d13793bc238cbf8fa248bcc2be2fd045965
julien-reboul <julien.reboul@bonitasoft.com>
2014-11-17 15:00:52 +0100

    fix protractor tests

--- test/e2e/features/admin/cases/case-tabs.e2e.js
+++ test/e2e/features/admin/cases/case-tabs.e2e.js
@@ -16,23 +16,21 @@
 
       activeCaseA = element(by.css('#TabActiveCases'));
       archivedCaseA = element(by.css('#TabArchivedCases'));
-      activeCaseTab = activeCaseA.getWebElement().findElement(by.xpath('..'));
-      archivedCaseTab = archivedCaseA.getWebElement().findElement(by.xpath('..'));
       //browser.debugger(); //launch protractor with debug option and use 'c' in console to continue test execution
     });
 
     it('should navigate between tabs', function(){
-      expect(element(by.css('#TabActiveCases')).getAttribute('class')).toContain('active');
-      expect(element(by.css('#TabArchivedCases')).getAttribute('class')).not.toContain('active');
+      expect(element(by.xpath('//li[a/@id="TabActiveCases"]')).getAttribute('class')).toContain('active');
+      expect(element(by.xpath('//li[a/@id="TabArchivedCases"]')).getAttribute('class')).not.toContain('active');
       archivedCaseA.click();
-      expect(element(by.css('#TabActiveCases')).getAttribute('class')).not.toContain('active');
-      expect(element(by.css('#TabArchivedCases')).getAttribute('class')).toContain('active');
+      expect(element(by.xpath('//li[a/@id="TabActiveCases"]')).getAttribute('class')).not.toContain('active');
+      expect(element(by.xpath('//li[a/@id="TabArchivedCases"]')).getAttribute('class')).toContain('active');
       archivedCaseA.click();
-      expect(element(by.css('#TabActiveCases')).getAttribute('class')).not.toContain('active');
-      expect(element(by.css('#TabArchivedCases')).getAttribute('class')).toContain('active');
+      expect(element(by.xpath('//li[a/@id="TabActiveCases"]')).getAttribute('class')).not.toContain('active');
+      expect(element(by.xpath('//li[a/@id="TabArchivedCases"]')).getAttribute('class')).toContain('active');
       activeCaseA.click();
-      expect(element(by.css('#TabActiveCases')).getAttribute('class')).toContain('active');
-      expect(element(by.css('#TabArchivedCases')).getAttribute('class')).not.toContain('active');
+      expect(element(by.xpath('//li[a/@id="TabActiveCases"]')).getAttribute('class')).toContain('active');
+      expect(element(by.xpath('//li[a/@id="TabArchivedCases"]')).getAttribute('class')).not.toContain('active');
 
     });
   });
